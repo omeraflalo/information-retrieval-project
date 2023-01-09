d7c5181227def58998f610fa33954823e5c215b5
Ariel-Pich <arielpich3@gmail.com>
2023-01-09 16:50:19 +0200

    index

---
+++ InvertedIndex.py
@@ -0,0 +1,194 @@
+from collections import defaultdict
+from contextlib import closing
+
+BLOCK_SIZE = 30
+TUPLE_SIZE = 6  # We're going to pack the doc_id and tf values in this many bytes.
+TF_MASK = 2 ** 16 - 1  # Masking the 16 low bits of an integer
+
+
+class MultiFileWriter:
+    """ Sequential binary writer to multiple files of up to BLOCK_SIZE each. """
+
+    def __init__(self, base_dir, name):
+        self._base_dir = Path(base_dir)
+        self._name = name
+        self._file_gen = (open(self._base_dir / f'{name}_{i:03}.bin', 'wb')
+                          for i in count())
+        self._f = next(self._file_gen)
+
+    def write(self, b):
+        locs = []
+        while len(b) > 0:
+            pos = self._f.tell()
+            remaining = BLOCK_SIZE - pos
+            # if the current file is full, close and open a new one.
+            if remaining == 0:
+                self._f.close()
+                self._f = next(self._file_gen)
+                pos, remaining = 0, BLOCK_SIZE
+            self._f.write(b[:remaining])
+            locs.append((self._f.name, pos))
+            b = b[remaining:]
+        return locs
+
+    def close(self):
+        self._f.close()
+
+
+class MultiFileReader:
+    """ Sequential binary reader of multiple files of up to BLOCK_SIZE each. """
+
+    def __init__(self):
+        self._open_files = {}
+
+    def read(self, locs, n_bytes):
+        b = []
+        for f_name, offset in locs:
+            if f_name not in self._open_files:
+                self._open_files[f_name] = open(f_name, 'rb')
+            f = self._open_files[f_name]
+            f.seek(offset)
+            n_read = min(n_bytes, BLOCK_SIZE - offset)
+            b.append(f.read(n_read))
+            n_bytes -= n_read
+        return b''.join(b)
+
+    def close(self):
+        for f in self._open_files.values():
+            f.close()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        return False
+
+
+class InvertedIndex:
+    def __init__(self, docs={}):
+        """ Initializes the inverted index and add documents to it (if provided).
+        Parameters:
+        -----------
+          docs: dict mapping doc_id to list of tokens
+        """
+        # stores document frequency per term
+        self.df = Counter()
+        # stores total frequency per term
+        self.term_total = Counter()
+        # stores posting list per term while building the index (internally),
+        # otherwise too big to store in memory.
+        self._posting_list = defaultdict(list)
+        # mapping a term to posting file locations, which is a list of
+        # (file_name, offset) pairs. Since posting lists are big we are going to
+        # write them to disk and just save their location in this list. We are
+        # using the MultiFileWriter helper class to write fixed-size files and store
+        # for each term/posting list its list of locations. The offset represents
+        # the number of bytes from the beginning of the file where the posting list
+        # starts.
+        self.posting_locs = defaultdict(list)
+
+        for doc_id, tokens in docs.items():
+            self.add_doc(doc_id, tokens)
+
+    def add_doc(self, doc_id, tokens):
+        """ Adds a document to the index with a given `doc_id` and tokens. It counts
+            the tf of tokens, then update the index (in memory, no storage
+            side-effects).
+        """
+        w2cnt = Counter(tokens)
+        self.term_total.update(w2cnt)
+        for w, cnt in w2cnt.items():
+            self.df[w] = self.df.get(w, 0) + 1
+            self._posting_list[w].append((doc_id, cnt))
+
+    def write(self, base_dir, name):
+        """ Write the in-memory index to disk and populate the `posting_locs`
+            variables with information about file location and offset of posting
+            lists. Results in at least two files:
+            (1) posting files `name`XXX.bin containing the posting lists.
+            (2) `name`.pkl containing the global term stats (e.g. df).
+        """
+        #### POSTINGS ####
+        self.posting_locs = defaultdict(list)
+        with closing(MultiFileWriter(base_dir, name)) as writer:
+            # iterate over posting lists in lexicographic order
+            for w in sorted(self._posting_list.keys()):
+                self._write_a_posting_list(w, writer, sort=True)
+        #### GLOBAL DICTIONARIES ####
+        self._write_globals(base_dir, name)
+
+    def _write_globals(self, base_dir, name):
+        with open(Path(base_dir) / f'{name}.pkl', 'wb') as f:
+            pickle.dump(self, f)
+
+    def _write_a_posting_list(self, w, writer, sort=False):
+        # sort the posting list by doc_id
+        pl = self._posting_list[w]
+        if sort:
+            pl = sorted(pl, key=itemgetter(0))
+        # convert to bytes
+        b = b''.join([(doc_id << 16 | (tf & TF_MASK)).to_bytes(TUPLE_SIZE, 'big')
+                      for doc_id, tf in pl])
+        # write to file(s)
+        locs = writer.write(b)
+        # save file locations to index
+        self.posting_locs[w].extend(locs)
+
+    def __getstate__(self):
+        """ Modify how the object is pickled by removing the internal posting lists
+            from the object's state dictionary.
+        """
+        state = self.__dict__.copy()
+        del state['_posting_list']
+        return state
+
+    @staticmethod
+    def read_index(base_dir, name):
+        with open(Path(base_dir) / f'{name}.pkl', 'rb') as f:
+            return pickle.load(f)
+
+    @staticmethod
+    def delete_index(base_dir, name):
+        path_globals = Path(base_dir) / f'{name}.pkl'
+        path_globals.unlink()
+        for p in Path(base_dir).rglob(f'{name}_*.bin'):
+            p.unlink()
+
+    def posting_lists_iter(self):
+        """ A generator that reads one posting list from disk and yields
+            a (word:str, [(doc_id:int, tf:int), ...]) tuple.
+        """
+        with closing(MultiFileReader()) as reader:
+            for w, locs in self.posting_locs.items():
+                # read a certain number of bytes into variable b
+                b = reader.read(locs, self.df[w] * TUPLE_SIZE)
+                posting_list = []
+                # convert the bytes read into `b` to a proper posting list.
+                lst = list(b)
+                for i in range(3, len(lst), 6):
+                    posting_list.append((lst[i], lst[i + 2]))
+                yield w, posting_list
+
+    def merge_indices(self, base_dir, names, output_name):
+        """ A function that merges the (partial) indices built from subsets of
+            documents, and writes out the merged posting lists.
+        Parameters:
+        -----------
+            base_dir: str
+                Directory where partial indices reside.
+            names: list of str
+                A list of index names to merge.
+            output_name: str
+                The name of the merged index.
+        """
+        indices = [InvertedIndex.read_index(base_dir, name) for name in names]
+        iters = [idx.posting_lists_iter() for idx in indices]
+        self.posting_locs = defaultdict(list)
+        #### POSTINGS: merge & write out ####
+        for i in iters:
+            for j in i:
+                self._posting_list[j[0]] += j[1]
+            self.write(base_dir, output_name)
+        for index in indices:
+            self.df += index.df
+            self.term_total += index.term_total
+            #### GLOBAL DICTIONARIES ####
+        self._write_globals(base_dir, output_name)
